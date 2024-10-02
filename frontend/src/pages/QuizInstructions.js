a3bdb3b8b6c6fb9917d4a3693535d401200649c0
imddevaraj <imddevaraj@gmail.com>
2024-10-03 01:16:09 +0530

    Added instructions

--- frontend/src/pages/QuizInstructions.js
+++ frontend/src/pages/QuizInstructions.js
@@ -15,14 +15,14 @@ function QuizInstructions() {
     <div className="quiz-instructions">
    
       <div className="instructions-content">
-        <h2>Quiz Instructions</h2>
+        <h2>Fast Fingers Instructions</h2>
         <p>Please read the following instructions carefully before starting the quiz:</p>
         <ul>
-          <li>Welcome to the Connection Game! You will see a sequence of images that represent words or phrases.  </li>
-          <li>Your goal is to figure out the word or phrase by analyzing the images.</li>
-          <li>Type your answer in the provided text box.</li>
-          <li>Make sure you save your progress, If in case you need break</li>
+          <li>Welcome to the Fast Fingers Game! You will have 20 Questions to answer in 20 min.  </li>
+          <li>Your goal is to answer as much as questions in the time limit.</li>
+          <li>The quiz automatically closes after the time expires and answers will get submitted.</li>
           <li>If you leave the quiz mid-way without save, your progress will be lost.</li>
+          <li>Good luck!</li>
         </ul>
         <button onClick={startQuiz} className="btn start-btn">Start Quiz</button>
       </div>
